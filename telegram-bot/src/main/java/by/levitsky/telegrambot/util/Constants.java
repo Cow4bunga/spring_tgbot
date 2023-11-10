59eb359a466908431f2294ef27090b0300c5952c
Ivan L <levitskyi.1002@gmail.com>
2023-11-10 12:29:47 +0300

    minor fixes

--- telegram-bot/src/main/java/by/levitsky/telegrambot/util/Constants.java
+++ telegram-bot/src/main/java/by/levitsky/telegrambot/util/Constants.java
@@ -16,7 +16,16 @@ public class Constants {
     public static final String HEAD_SRC="HEAD_SRC";
     public static final String SOURCE_CAT="SOURCE_CAT";
     public static final String SOURCE_CTR="SOURCE_CTR";
+    public static final String ADD="ADD";
+    public static final String EDIT="EDIT";
+    public static final String DELETE="DELETE";
+
     public static final String YES_BUTTON = "YES_BTN";
     public static final String NO_BUTTON = "NO_BTN";
-
+    public static final String HELP_MESSAGE = "You can execute commands from the main menu on the left or by typing a command:\n\n" +
+            "Type /start to see a welcome message\n\n" +
+//            "Type /stats to see statistics for your data\n\n" +
+            "Type /data to see the contents of your notes\n\n" +
+            "Type /delete to remove note/-s from your data\n\n" +
+            "Type /help to see this message again";
 }
